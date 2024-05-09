7db7525d812c55bcab8c45010e5227fac3318eb7
GtzRob <98448290+GtzRob@users.noreply.github.com>
2024-05-09 16:19:04 -0600

    Update index.php

--- secciones/puestos/index.php
+++ secciones/puestos/index.php
@@ -13,7 +13,7 @@ if(isset($_GET['txtID'])){
 }
 
 
-$sentencia=$conexion->prepare("SELECT * FROM `tbl_puestos`");
+$sentencia=$conexion->prepare("SELECT * FROM tbl_puestos");
 $sentencia->execute();
 $lista_tbl_puestos=$sentencia->fetchAll(PDO::FETCH_ASSOC);
 
